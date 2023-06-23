ae082a28f0a64f6251efa483b51495ee4031a45d
Lucy de Rojas <lucyderojas@gmail.com>
2023-06-23 08:45:53 +0100

    'update'

---
+++ src/dataPostInterface.ts
@@ -0,0 +1,12 @@
+export interface DataPost {
+        id: number;
+        image: string;
+        alt:string;
+        title: string;
+        descriptionShort: string;
+        descriptionFull: string;
+        topic: string;
+        user: string;
+        date: string;
+        time: string;
+}
